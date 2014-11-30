25bdc2f463bd11a4eac1e9f97e35580b38ad23e0
juan2ramos <juan2ramos@gmail.com>
2014-11-29 19:41:24 -0500

    + form y fecha

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>
-<html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
+
 <html lang="es">
 <head>
 
-    <title>Drawde !! Juan2ramos</title>
+    <title>Aguardiente Llanero</title>
 
     <!-- Meta -->
     <meta charset="utf-8">
@@ -23,63 +17,104 @@
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic'
           rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/style.css"/>
-    <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
 
-    <!-- Humans -->
-    <link type="text/plain" rel="author" href="humans.txt"/>
 
 </head>
 <body>
-    <div>
-        <button id="form21" onclick="empezar();">Empezar</button>    
-    </div>
-    
-    <div>
-        <input type="text"  placeholder="Nombre" class="hiden" id="form"/>   <br></br>
-        <input type="text"  placeholder="Apellido" class="hiden" id="form1"/> <br></br>
-        <input type="text"  placeholder="Edad" class="hiden" id="form2"/> <br></br>
-        <input type="text"  placeholder="Ocupación" class="hiden" id="form3"/> <br></br>
-        <input type="email"  placeholder="Correo electronico" class="hiden" id="form4"/> <br></br>  
-        <input type="text"  placeholder="Teléfono" class="hiden" id="form5"/> <br></br>
-        <input type="text"  placeholder="Celular" class="hiden" id="form6"/> <br></br>
-        <label class="hiden" id="form7">Actualmente cursa alguna carrera técnica, tecnológica o profesional?</label>
-        <select class="hiden" id="form8">
-            <option value="Si">Si</option>
-            <option value="No">No</option>
-        </select > <br></br>
-        <label class="hiden" id="form9">Tienes algún título universitario?</label>
-        <select class="hiden" id="form10">
-            <option value="Si">Si</option>
-            <option value="No">No</option>
-        </select > <br></br>
-        <label class="hiden" id="form11">Eres llanera de nacimiento o has vivido en el Meta los últimos 5 años?</label>
-        <select class="hiden" id="form12">
-            <option value="Si">Si</option>
-            <option value="No">No</option>
-        </select> <br></br>
-        <label class="hiden" id="form13">Tienes conocimientos sobre la cultura llanera y sus costumbres?</label>
-        <select class="hiden" id="form14">
-            <option value="Si">Si</option>
-            <option value="No">No</option>
-        </select> <br></br>
-        <label class="hiden" id="form15">Te desenvuelves bien ante cámaras y eres expresiva? </label>
-        <select class="hiden" id="form16">
-            <option value="Si">Si</option>
-            <option value="No">No</option>
-        </select> <br></br>
-        <label class="hiden" id="form17">Estas dispuesta a viajar y promocionar la marca Aguardiente Llanero durante 1 año en todo el territorio nacional? </label>
-        <select class="hiden" id="form18">
-            <option value="Si">Si</option>
-            <option value="No">No</option>
-        </select> <br></br>
+<div id="wrapper">
+    <figure><img src="img/a-lo-que-vinimos.png" alt="a lo que vinimos"/></figure>
+    <form action="" id="form-girls">
+        <p>
+            Hola, Ingresa tus datos en el siguiente formulario, carga tu fotografía y participa por ser la chica llanero
+            2015. enseñale al mundo que hace de nuestros llanos orientales el lugar mas hermoso del Pais con las mujeres
+            mas
+            lindas... entonces.. <b> ¡A lo que vinimos, Vamos!</b>
+        </p>
 
-        <button   id="form19" class="hiden" >Regritrarse</button>
-        <button id="form20" onclick="empezar();" class="hiden">siguiente</button>
-    </div>
-    
+        <div id="content-1" class="content-questions">
+            <input type="text" placeholder="Nombre" name="nombre"/>
+            <input type="text" placeholder="Apellido" name="apellido"/>
+            <input type="text" placeholder="Edad" name="edad"/>
+        </div>
+        <div id="content-2" class="content-questions">
+            <input type="text" placeholder="Ocupación" name="ocupacion"/>
+            <input type="email" placeholder="Correo electronico" name="mail"/>
+            <input type="text" placeholder="Teléfono" name="telefono"/>
+            <input type="text" placeholder="Celular" name="celular"/>
 
-</body>
+        </div>
+        <div id="content-3" class="content-questions">
+
+            <label>Actualmente cursa alguna carrera técnica, tecnológica o profesional?
+                <select name="">
+                    <option value="Si">Si</option>
+                    <option value="No">No</option>
+                </select>
+            </label>
+
+            <label>Tienes algún título universitario?
+                <select>
+                    <option value="Si">Si</option>
+                    <option value="No">No</option>
+                </select>
+            </label>
+            <label>Eres llanera de nacimiento o has vivido en el Meta los últimos 5 años?
+                <select>
+                    <option value="Si">Si</option>
+                    <option value="No">No</option>
+                </select>
+            </label>
+        </div>
+        <div id="content-4" class="content-questions">
+
+            <label>Tienes conocimientos sobre la cultura llanera y sus costumbres?
+                <select>
+                    <option value="Si">Si</option>
+                    <option value="No">No</option>
+                </select>
+            </label>
+            <label>Te desenvuelves bien ante cámaras y eres expresiva?
+                <select>
+                    <option value="Si">Si</option>
+                    <option value="No">No</option>
+                </select>
+            </label>
+            <label>Estas dispuesta a viajar y promocionar la marca Aguardiente Llanero durante 1 año en
+                todo el territorio nacional?
+                <select>
+                    <option value="Si">Si</option>
+                    <option value="No">No</option>
+                </select>
+            </label>
+            <button>Registrarse</button>
+        </div>
+        <div class="content-button">
+            <button id="prev">Anterior</button>
+            <button id="next" class="show-inline">siguiente</button>
+        </div>
+    </form>
+    <div class="terms">
+        * Terminos y condiciones | * Bases del concurso
+    </div>
+</div>
+
+<div class="date-form-content" id="date-form-content">
+    <div class="date-form">
+        <input type="text" placeholder="D" id="day"/>
+        <input type="text" placeholder="M" id="month"/>
+        <input type="text" placeholder="A" id="year"/>
+
+        <p>*Debes ser mayor de edad para ingresar.</p>
+
+        <div class="content-button">
+            <button id="date-submit">Ingresar</button>
+        </div>
+        <figure><img src="img/a-lo-que-vinimos.png" alt=""/></figure>
+    </div>
+</div>
 <!-- JavaScript -->
-
 <script src="js/script.js"></script>
+</body>
+
+
 </html>
--- js/script.js
+++ js/script.js
@@ -1,107 +1,63 @@
+var d = document,
+    next = d.getElementById("next"),
+    contentQuestionAll = d.querySelectorAll('.content-questions'),
+    dateSubmit = d.getElementById('date-submit'),
+    numberContentQ = contentQuestionAll.length,
+    actual = 1;
+d.getElementById("content-" + actual).classList.add('show-questions');
+next.addEventListener('click', nextQuestion);
+prev.addEventListener('click', prevQuestion);
+dateSubmit.addEventListener('click', dateCheck);
+function nextQuestion(e) {
 
-var contador=0;
-	var borrar=document.getElementById("form");
-	var borrar1=document.getElementById("form1");
-	var borrar2=document.getElementById("form2");
-	var borrar3=document.getElementById("form3");
-	var borrar4=document.getElementById("form4");
-	var borrar5=document.getElementById("form5");
-	var borrar6=document.getElementById("form6");
-	var borrar7=document.getElementById("form7");
-	var borrar8=document.getElementById("form8");
-	var borrar9=document.getElementById("form9");
-	var borrar10=document.getElementById("form10");
-	var borrar11=document.getElementById("form11");
-	var borrar12=document.getElementById("form12");
-	var borrar13=document.getElementById("form13");
-	var borrar14=document.getElementById("form14");
-	var borrar15=document.getElementById("form15");
-	var borrar16=document.getElementById("form16");
-	var borrar17=document.getElementById("form17");
-	var borrar18=document.getElementById("form18");
-	var borrar19=document.getElementById("form19");
-	var borrar20=document.getElementById("form20");
-	var borrar21=document.getElementById("form21");
+    e.preventDefault();
+    d.getElementById("content-" + actual).classList.remove('show-questions');
+    actual++;
+    d.getElementById("content-" + actual).classList.add('show-questions');
+    if (actual == numberContentQ) {
+        next.classList.remove('show-inline')
+    }
+    if (actual == 2) {
+        prev.classList.add('show-inline')
+    }
 
-function empezar(){
-	borrar21.classList.add('hiden');
-	borrar20.classList.remove('hiden');
-	
+}
+function prevQuestion(e) {
 
-	if(contador===0){
-		borrar.classList.remove('hiden');
-	}
-
-	if(contador===1){
-		borrar1.classList.remove('hiden');
-		borrar.classList.add('hiden');
-	}
-	if(contador===2){
-		borrar2.classList.remove('hiden');
-		borrar1.classList.add('hiden');
-	}
-	if(contador===3){
-		borrar3.classList.remove('hiden');
-		borrar2.classList.add('hiden');
-	}
-	if(contador===4){
-		borrar4.classList.remove('hiden');
-		borrar3.classList.add('hiden');
-	}
-	if(contador===5){
-		borrar5.classList.remove('hiden');
-		borrar4.classList.add('hiden');
-	}
-	if(contador===6){
-		borrar6.classList.remove('hiden');
-		borrar5.classList.add('hiden');
-	}
-	if(contador===7){
-		borrar8.classList.remove('hiden');
-		borrar7.classList.remove('hiden');
-		borrar6.classList.add('hiden');
-	}
-	if(contador===8){
-		borrar10.classList.remove('hiden');
-		borrar9.classList.remove('hiden');
-		borrar8.classList.add('hiden');
-		borrar7.classList.add('hiden');
-	}
-	if(contador===9){
-		borrar12.classList.remove('hiden');
-		borrar11.classList.remove('hiden');
-		borrar10.classList.add('hiden');
-		borrar9.classList.add('hiden');
-	}
-	if(contador===10){
-		borrar14.classList.remove('hiden');
-		borrar13.classList.remove('hiden');
-		borrar12.classList.add('hiden');
-		borrar11.classList.add('hiden');
-	}
-	if(contador===11){
-		
-		borrar16.classList.remove('hiden');
-		borrar15.classList.remove('hiden');
-		borrar14.classList.add('hiden');
-		borrar13.classList.add('hiden');
-	}
-	if(contador===12){
-	
-		borrar18.classList.remove('hiden');
-		borrar17.classList.remove('hiden');
-		borrar16.classList.add('hiden');
-		borrar15.classList.add('hiden');
-	}
-
-	if(contador===13){
-		borrar19.classList.remove('hiden');
-		borrar20.classList.add('hiden');
-		borrar18.classList.add('hiden');
-		borrar17.classList.add('hiden');
-	}
-	
-	contador++;
-	}
+    e.preventDefault();
+    d.getElementById("content-" + actual).classList.remove('show-questions');
+    actual--;
+    d.getElementById("content-" + actual).classList.add('show-questions');
+    if (actual == numberContentQ - 1) {
+        next.classList.add('show-inline')
+    }
+    if (actual == 1) {
+        prev.classList.remove('show-inline')
+    }
+}
+function dateCheck(e) {
+    e.preventDefault();
+    var dayInput = document.getElementById("day").value;
+    var monthInput = document.getElementById("month").value;
+    var yearInput = document.getElementById("year").value;
 
 
+    if (parseFloat(dayInput) != dayInput || parseFloat(monthInput) != monthInput || parseFloat(yearInput) != yearInput) {
+        alert(' Fecha invalida');
+    } else {
+        var birthday = new Date(yearInput, monthInput, dayInput);
+        var today = new Date();
+        var years = today.getFullYear() - birthday.getFullYear();
+        birthday.setFullYear(today.getFullYear());
+        years = (today < birthday) ? years-- : years;
+
+        if (years >= 18) {
+            var contentDate = d.getElementById("date-form-content")
+            contentDate.classList.add('hidden')
+        } else {
+            alert('Debes ser mayor de edad');
+        }
+    }
+
+
+}
