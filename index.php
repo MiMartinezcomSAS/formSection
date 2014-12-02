44bbe24b0bb8e452d343973f1887223e912e753e
Michael Martinez <soporte@mi-martinez.com>
2014-12-02 13:17:37 -0500

    Revert "Analitics"
    This reverts commit b90d696d372df70d5f5028cb2aac5c468135e445.

--- index.php
+++ index.php
@@ -50,16 +50,8 @@ function validarPost($input)
       <meta property="og:image" content="http://aguardientellanero.com/img/facebook.png" />
       <meta property="og:description" content="Te invitamos a representar la cultura del llano en un concurso que resalta la mujer llanera." />
       <meta property="og:url" content="http://aguardientellanero.com" />
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-52560577-12', 'auto');
-  ga('send', 'pageview');
 
-</script>
 </head>
 <body>
 <?php if ($error): ?>
